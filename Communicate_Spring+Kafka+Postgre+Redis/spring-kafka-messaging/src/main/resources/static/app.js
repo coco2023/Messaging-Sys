592c01b09baf092bc3a01693181f5911fa99ed73
keyuhan <hankeyu2021@gmail.com>
2023-07-20 22:38:45 -0500

    comments on ajax

--- Communicate_Spring+Kafka+Postgre+Redis/spring-kafka-messaging/src/main/resources/static/app.js
+++ Communicate_Spring+Kafka+Postgre+Redis/spring-kafka-messaging/src/main/resources/static/app.js
@@ -1,16 +1,19 @@
+// $("#elementId"): select elements by their IDs
+// $.ajax({ ... });
+
 function activate() {
   var mobileFormData = JSON.stringify({
     'mobile': $("#mobile").val(),
-  });
+  }); // -- ActivationRequest
 
   console.log("here we are")
   $.ajax({
     type: "POST",
     url: "http://localhost:9001/api/auth/getcode",
-    data: mobileFormData,
+    data: mobileFormData,   // ActivationRequest
     contentType: "application/json;charset=utf-8",
-    success: function(result){
-        $("#activationCode").val(result.activationCode);
+    success: function(activationResponse){      // ActivationResponse
+        $("#activationCode").val(activationResponse.activationCode);
     }
   });
 }
@@ -19,15 +22,16 @@ function login() {
   var loginFormData = JSON.stringify({
     'mobile': $("#mobile").val(),
     'activationCode': $("#activationCode").val(),
-  });
+  });  // -- LoginRequest
 
   $.ajax({
     type: "POST",
     url: "http://localhost:9001/api/auth/login",
-    data: loginFormData,
+    data: loginFormData,    // -- LoginRequest
     contentType: "application/json;charset=utf-8",
-    success: function(result){
-        $("#accessToken").val(result.accessToken);
+    success: function(LoginResponse){  // will exec when server returns a successful response;
+        // result: the response data from the server -- LoginResponse
+        $("#accessToken").val(LoginResponse.accessToken);
     }
   });
 }
@@ -35,17 +39,17 @@ function login() {
 function loadContacts() {
   var getcontactsFormData = JSON.stringify({
     'accessToken': $("#accessToken").val(),
-  });
+  });  // -- ContactRequest
 
   $("#contactlist").empty();
   $.ajax({
     type: "POST",
     url: "http://localhost:9001/api/auth/getcontacts",
-    data: getcontactsFormData,
+    data: getcontactsFormData,   // it is ContactRequest
     contentType: "application/json;charset=utf-8",
-    success: function(result){
-        console.log("here the contacts: ", JSON.stringify(result));
-        result.contacts.forEach(function(c) {
+    success: function(contactResponse){   // response after successful request -- return contactResponse
+        console.log("here the contacts: ", JSON.stringify(contactResponse));
+        contactResponse.contacts.forEach(function(c) {
             console.log(c.user.mobile);
             $("#contactlist").append('<tr><td><input type="checkbox" id="sendTo" value="' + c.contactUserId + '">' + c.user.mobile + '</input></td></tr>');
         });
@@ -53,20 +57,13 @@ function loadContacts() {
   });
 }
 
-// credits: https://www.javainuse.com/spring/boot-websocket
-var ws;
-function setConnected(connected) {
-	$("#connect").prop("disabled", connected);
-	$("#disconnect").prop("disabled", !connected);
-	if(connected) {
-	    loadContacts();
-	    $("#chatbox").show();
-	} else {
-	    $("#chatbox").hide();
-	}
-}
-
+/**
+ * connect(): this method establishes a WebSocket connection to the server.
+ * WebSockets allow bi-directional communication between the client and server,
+ * enabling real-time updates and messaging
+ */
 function connect() {
+    // creates a new WebSocket instance and connects to the server using the specified WebSocket URL
 	ws = new WebSocket('ws://localhost:9001/messaging?accessToken=' + $("#accessToken").val());
     ws.addEventListener('error', (error) => {
 		console.log("Error: ", error.message);
@@ -76,11 +73,34 @@ function connect() {
 		console.log("Close: ", event.code + " - " + event.reason);
 	    setConnected(false);
     })
+    // the event handler for receiving helloWorld messages from the WebSocket server; test
 	ws.onmessage = function(event) {
 		helloWorld(event.data);
 		console.log("Connected to websocket " + event.data);
 	}
+    // sets the connection status to true
 	setConnected(true);
+}
+
+// credits: https://www.javainuse.com/spring/boot-websocket
+/**
+ * represents the status of the WebSocket connection
+ * @param connected
+ */
+// `ws` is used to store the WebSocket instance created when connecting to the server
+var ws;
+function setConnected(connected) {
+    // set the "disabled" property of the element with the ID "connect" ($("#connect") button) to the value of the `connected` parameter
+    $("#connect").prop("disabled", connected);      // When `connected` is true, it disables the $("#connect") button
+    // set the "disabled" property of the element with the ID "disconnect" ($("#disconnect") button) to the OPPOSITE of the `connected` parameter
+    $("#disconnect").prop("disabled", !connected);  // When connected is true, it enables the "Disconnect" button &
+                                                          // & allowing the user to click it to disconnect from the WebSocket server
+    if(connected) {
+        loadContacts();
+        $("#chatbox").show();
+    } else {
+        $("#chatbox").hide();
+    }
 }
 
 function disconnect() {
@@ -108,6 +128,12 @@ function helloWorld(message) {
 	$("#chatmessages").append("<tr><td>" + message + "</td></tr>");
 }
 
+/**
+ *  event listeners for 5 different buttons with IDs:
+ *      "activate", "login", "connect", "disconnect", "send".
+ *  When each button is clicked, it calls the corresponding function:
+ *      activate(), login(), connect(), disconnect(), sendData().
+ */
 $(function() {
 	$("form").on('submit', function(e) {
 		e.preventDefault();
